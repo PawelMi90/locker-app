c5e216f3ad72fc4e44582b95b916a4665464b5f4
Pawel Michalczuk <paw.michalczuk@gmail.com>
2022-04-03 15:06:02 +0200

    Initial commit

---
+++ src/main/java/com/example/lockerapp/LockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.lockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lockerapp/LockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
